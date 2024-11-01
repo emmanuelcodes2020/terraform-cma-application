e345b3e9fad812051be03a6a1c973bcd099a6b45
Emmanuel Yewande <emmanuel.yewande@outlook.com>
2024-11-01 00:13:14 +0000

    cma app modified

--- sg.tf
+++ sg.tf
@@ -39,11 +39,11 @@ resource "aws_security_group_rule" "ingress_ssh_access" {
 
 
 
-resource "aws_security_group_rule" "ingress_http_access" {
+resource "aws_security_group_rule" "ingress_tcp_access" {
   type              = "ingress"
-  description       = "HTTP access"
-  from_port         = 80
-  to_port           = 80
+  description       = "Custom TCP access"
+  from_port         = 8081
+  to_port           = 8081
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.cma_autoscaling_sg.id
